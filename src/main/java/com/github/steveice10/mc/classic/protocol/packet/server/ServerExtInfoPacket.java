00ac8b5984d12f977f12be93172e933e41e1d948
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-01 23:18:25 -0600

    Create ServerExtInfoPacket.java

---
+++ src/main/java/com/github/steveice10/mc/classic/protocol/packet/server/ServerExtInfoPacket.java
@@ -0,0 +1,40 @@
+package com.github.steveice10.mc.classic.protocol.packet.server;
+
+import com.github.steveice10.mc.classic.protocol.packet.ClassicPacketUtil;
+import com.github.steveice10.packetlib.io.NetInput;
+import com.github.steveice10.packetlib.io.NetOutput;
+import com.github.steveice10.packetlib.packet.Packet;
+
+import java.io.IOException;
+
+public class ServerExtInfoPacket implements Packet{
+    private String appName;
+    private short extensionCount;
+    @SuppressWarnings("unused")
+	private ServerExtInfoPacket() {
+    }
+    public ServerExtInfoPacket(String appName, short extensionCount){
+        this.appName = appName;
+        this.extensionCount = extensionCount;
+    }
+    public String getAppName(){
+        return this.appName;
+    }
+    public short getExtensionCount(){
+        return this.extensionCount;
+    }
+    @Override
+    public void read(NetInput in) throws IOException {
+        this.appName = ClassicPacketUtil.readString(in);
+        this.extensionCount = in.readShort();
+    }
+    @Override
+	public void write(NetOutput out) throws IOException {
+        ClassicPacketUtil.writeString(out, this.appName);
+        out.writeShort(this.extensionCount);
+    }
+    @Override
+	public boolean isPriority() {
+		return false;
+    }
+}
